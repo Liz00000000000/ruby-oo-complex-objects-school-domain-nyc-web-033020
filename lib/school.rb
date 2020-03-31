89e8ff43e25f471c29abb2d5aff3d3d9e961d163
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-03-31 22:49:45 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
+#binding.pry
 # code here!
+class School
+  def roster
+    roster = [ ]
+  end
+
+  def add_student(name)
+    roster << name
+  end
+
+  def grade
+  end
+
+  def sort
+    roster.sort 
+  end
+
+end
+
+#School.roster()
